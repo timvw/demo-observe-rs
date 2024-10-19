aa423eb03fa051a9e697fe2fc5a2d7e502376fbb
Tim Van Wassenhove <github@timvw.be>
2024-10-19 12:47:50 +0200

    feat: add basic axum server

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,6 +1,8 @@
 mod otel;
 
 use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::signal;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,9 +16,55 @@ async fn main() -> Result<()> {
     let logger_provider = otel::init_logger_provider()?;
     let meter_provider = otel::init_meter_provider()?;
 
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health))
+        .route("/error", get(error));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
     meter_provider.shutdown()?;
     logger_provider.shutdown()?;
     tracer_provider.shutdown()?;
 
     Ok(())
+}
+
+async fn root() -> Result<&'static str, StatusCode> {
+    Ok("root")
+}
+
+async fn health() -> Result<&'static str, StatusCode> {
+    Ok("UP")
+}
+
+async fn error() -> Result<&'static str, StatusCode> {
+    Err(StatusCode::SERVICE_UNAVAILABLE)
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
