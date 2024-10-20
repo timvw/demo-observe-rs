faca25a25790f304a8767a6feb550f5f533d5f0a
Tim Van Wassenhove <github@timvw.be>
2024-10-20 17:10:54 +0200

    feat: demonstrate context propagation

--- src/server/main.rs
+++ src/server/main.rs
@@ -2,15 +2,23 @@ mod otel;
 
 use anyhow::Result;
 use axum::body::Body;
+use axum::extract::State;
 use axum::{http::StatusCode, routing::get, Router};
-use http::Request;
+use http::{HeaderMap, Request};
 use opentelemetry::trace::{TraceContextExt, Tracer};
 use opentelemetry::{global, Context};
-use opentelemetry_http::HeaderExtractor;
+use opentelemetry_http::{HeaderExtractor, HeaderInjector};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_tracing::TracingMiddleware;
 use tokio::signal;
-use tower_http::trace::TraceLayer;
-use tracing::Span;
+use tower_http::trace::{DefaultMakeSpan, MakeSpan, TraceLayer};
+use tracing::{info, Span};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
+
+#[derive(Clone, Debug)]
+pub struct ServerState {
+    http_client: ClientWithMiddleware,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -24,7 +32,7 @@ async fn main() -> Result<()> {
     let logger_provider = otel::init_logger_provider()?;
     let meter_provider = otel::init_meter_provider()?;
 
-    let on_request_handler = |req: &Request<Body>, span: &Span| {
+    let span_builder = |req: &Request<Body>| {
         let parent_context = global::get_text_map_propagator(|propagator| {
             propagator.extract(&HeaderExtractor(req.headers()))
         });
@@ -32,16 +40,27 @@ async fn main() -> Result<()> {
         let otel_span_name = format!("{} {}", req.method(), req.uri().path());
         let otel_span = otel_tracer.start_with_context(otel_span_name, &parent_context);
         let cx = Context::current_with_span(otel_span);
+        let span = DefaultMakeSpan::default().make_span(req);
         span.set_parent(cx);
+        span
     };
 
-    let trace_layer = TraceLayer::new_for_http().on_request(on_request_handler);
+    let trace_layer = TraceLayer::new_for_http().make_span_with(span_builder);
+
+    let client = ClientBuilder::new(reqwest::Client::new())
+        .with(TracingMiddleware::default())
+        .build();
+
+    let server_state = ServerState {
+        http_client: client,
+    };
 
     let app = Router::new()
         .route("/", get(root))
         .route("/health", get(health))
         .route("/error", get(error))
-        .layer(trace_layer);
+        .layer(trace_layer)
+        .with_state(server_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app)
@@ -55,7 +74,33 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn root() -> Result<&'static str, StatusCode> {
+fn build_request_headers() -> HeaderMap {
+    let context = Span::current().context();
+    let mut req_headers = HeaderMap::new();
+
+    global::get_text_map_propagator(|propagator| {
+        propagator.inject_context(&context, &mut HeaderInjector(&mut req_headers))
+    });
+
+    req_headers
+}
+
+async fn root(State(server_state): State<ServerState>) -> Result<&'static str, StatusCode> {
+    let req_headers = build_request_headers();
+
+    let health_rsp = server_state
+        .http_client
+        .get("http://localhost:3000/health")
+        .headers(req_headers)
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let body = health_rsp
+        .text()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    info!("we received {}", body);
+
     Ok("root")
 }
 
