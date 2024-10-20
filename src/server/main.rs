d9de3f78b91eb7b5cd895b4de4600bd22dbdf27e
Tim Van Wassenhove <github@timvw.be>
2024-10-20 16:45:32 +0200

    feat: configure tower trace layer to be wrapped by an otel trace which shows some extra info

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,9 +1,16 @@
 mod otel;
 
 use anyhow::Result;
+use axum::body::Body;
 use axum::{http::StatusCode, routing::get, Router};
+use http::Request;
+use opentelemetry::trace::{TraceContextExt, Tracer};
+use opentelemetry::{global, Context};
+use opentelemetry_http::HeaderExtractor;
 use tokio::signal;
 use tower_http::trace::TraceLayer;
+use tracing::Span;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,11 +24,24 @@ async fn main() -> Result<()> {
     let logger_provider = otel::init_logger_provider()?;
     let meter_provider = otel::init_meter_provider()?;
 
+    let on_request_handler = |req: &Request<Body>, span: &Span| {
+        let parent_context = global::get_text_map_propagator(|propagator| {
+            propagator.extract(&HeaderExtractor(req.headers()))
+        });
+        let otel_tracer = global::tracer("");
+        let otel_span_name = format!("{} {}", req.method(), req.uri().path());
+        let otel_span = otel_tracer.start_with_context(otel_span_name, &parent_context);
+        let cx = Context::current_with_span(otel_span);
+        span.set_parent(cx);
+    };
+
+    let trace_layer = TraceLayer::new_for_http().on_request(on_request_handler);
+
     let app = Router::new()
         .route("/", get(root))
         .route("/health", get(health))
         .route("/error", get(error))
-        .layer(TraceLayer::new_for_http());
+        .layer(trace_layer);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app)
